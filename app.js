11a380072bfb944123c006dffa911575ab309478
misterfancypantsfancy <easyemail112233@protonmail.com>
2023-05-14 16:39:56 -0400

    WIP: curled some routes

---
+++ app.js
@@ -0,0 +1,26 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello from /hello route!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
