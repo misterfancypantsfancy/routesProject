ddfa4daf35369a80da3b44c499100d71ab9cd527
misterfancypantsfancy <easyemail112233@protonmail.com>
2023-05-20 10:10:41 -0400

    WIP: separating server side and client side

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // const express = require('express');
 // const app = express();
 
@@ -5,20 +6,25 @@
 //   res.send('Hello, Express!');
 // });
 
+// app.get('/hello', (req, res) => {
+//   res.send('Hello from /hello route!');
+// });
+
 // app.listen(3000, () => {
 //   console.log('Server started on port 3000');
 // });
 
 
+
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
+app.use(cors()); // Enable CORS for all routes
 
-app.get('/hello', (req, res) => {
-  res.send('Hello from /hello route!');
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from server-side API!' });
 });
 
 app.listen(3000, () => {
